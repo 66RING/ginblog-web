ca6f5651deb230a07be1199bf7528519f83e2779
66RING <1552087953@qq.com>
2020-08-21 22:31:41 +0800

    CateList and ArtList

--- web/admin/src/components/article/ArtList.vue
+++ web/admin/src/components/article/ArtList.vue
@@ -1,5 +1,243 @@
 <template>
   <div>
     <h1>文章列表页面</h1>
+    <a-card>
+      <a-row :gutter="20">
+        <a-col :span="6">
+          <a-input-search 
+            v-model="queryParam.title"
+            placeholder="Enter Art Name to Search"
+            enter-button
+            allowClear
+            @search="getArtList"
+          />
+        </a-col>
+        <a-col :span="4">
+          <a-button type="primary" @click="addArtVisible = true" @ok="addArtOk" @cancel="addArtCancel">New Art</a-button>
+        </a-col>
+
+        <a-col :span="6" :offset="4">
+          <a-select defaultValue="Search Cate" style="width:200px" @change="CateChange">
+            <a-select-option 
+              v-for="item in catelist"
+              :key="item.id" 
+              :value="item.id"
+              >{{item.name}}</a-select-option>
+          </a-select>
+        </a-col>
+      </a-row>
+
+      <a-table 
+        rowKey="ID"
+        :columns="columns"
+        :pagination="pagination"
+        :dataSource="artlist"
+        bordered
+        @change="handleTableChange"
+        >
+        <span class="ArtImg" slot="img" slot-scope="img">
+          <img :src="img"/>
+        </span>
+        <template slot="action" slot-scope="data">
+          <div class="actionSlot">
+            <a-button type="primary" size="small" style="margin-right:15px" @click="editArt(data.ID)">Edit</a-button>
+            <a-button type="danger" size="small" style="margin-right:15px" @click="deleteArt(data.ID)">Delete</a-button>
+          </div>
+        </template>
+      </a-table>
+    </a-card>
+
   </div>
 </template>
+
+
+<script>
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'ID',
+    width: '5%',
+    key: 'id',
+    align: 'center'
+  },
+  {
+    title: 'Category',
+    dataIndex: 'Category.name',
+    width: '10%',
+    key: 'name',
+    align: 'center'
+  },
+  {
+    title: 'Aricle Title',
+    dataIndex: 'title',
+    width: '10%',
+    key: 'role',
+    align: 'center',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'desc',
+    width: '20%',
+    key: 'desc',
+    align: 'center',
+  },
+  {
+    title: 'Image',
+    dataIndex: 'img',
+    width: '20%',
+    key: 'img',
+    scopedSlots: {customRender: 'img'},
+    align: 'center',
+  },
+  {
+    title: 'Action',
+    width: '15%',
+    key: 'action',
+    scopedSlots: {customRender: 'action'},
+    align: 'center',
+  }
+]
+
+export default{
+  data(){
+    return {
+      pagination: {
+        pageSizeOptions: ['5', '10', '20'],
+        pageSize: 5,
+        total: 0,
+        showSizeChanger: true,
+        showTotal: (total) => `共${total}条`,
+      },
+      artlist: [],
+      catelist: [],
+      columns,
+      queryParam: {
+        title: "",
+        pagesize: 5,
+        pagenum: 1,
+      },
+    }
+
+  },
+  created(){
+    this.getArtList()
+    this.getCateList()
+  },
+  methods: {
+    async getArtList(){
+      const {data: res} = await this.$http.get('article', {
+        params:{
+          title: this.queryParam.title,
+          pagesize: this.queryParam.pagesize,
+          pagenum: this.queryParam.pagenum,
+        },
+      })
+      if (res.status != 200) return this.$message.error(res.message)
+      this.artlist = res.data
+      this.pagination.total = res.total
+    },
+
+    // 重来
+    handleTableChange(pagination, filters, sorter){
+      var pager = { ...this.pagination }
+      pager.current = pagination.current
+      pager.pageSize = pagination.pageSize
+      this.queryParam.pagesize = pagination.pageSize
+      this.queryParam.pagenum = pagination.current
+
+      if(pagination.pageSize != this.pagination.pageSize){
+        this.queryParam.pagenum = 1
+        pager.current = 1
+      }
+      this.pagination = pager
+      this.getArtList()
+    },
+
+    // Delete Art
+    async deleteArt(id){
+      this.$confirm({
+        title: 'Do you want to delete this art?',
+        content: 'When clicked the OK button, there is no way back',
+        onOk: async () => {
+          const res = await this.$http.delete(`article/${id}`)
+          if (res.status != 200) return this.$message.error(res.message)
+          this.getArtList()
+          this.$message.success("Art has been delete!")
+        },
+        onCancel() {},
+      });
+    },
+
+    // Get Cate Info
+    async getCateList(){
+      const {data: res} = await this.$http.get('category')
+      if (res.status != 200) return this.$message.error(res.message)
+      this.catelist = res.data
+      this.pagination.total = res.total
+    },
+
+    // Cate Change
+    CateChange(value){
+      this.GetCateArt(value)
+    },
+    async GetCateArt(id){
+      const {data: res} = await this.$http.get(`article/list/${id}`)
+      if (res.status != 200) return this.$message.error(res.message)
+      this.artlist = res.data
+      this.pagination.total = res.total
+    },
+
+
+
+
+
+
+    // Add Art
+    addArtCancel(){
+      this.$refs.addArtRef.resetFields()
+      this.addArtVisible = false
+      this.$message.info("Add Cancel")
+    },
+    addArtOk(){
+      this.$refs.addArtRef.validate(async (valid) => {
+        if(!valid) return this.$message.error("Invalid")
+        const {data: res} = await this.$http.post("art/add", {
+          artname: this.newArt.artname,
+          password: this.newArt.password,
+          role: this.newArt.role
+        })
+        if (res.status !=200) return this.$message.error(res.message)
+        this.addArtVisible = false
+        this.$message.success("Add Art Success")
+        this.getArtList()
+      })
+    },
+    adminChange(checked){
+      if(checked){
+        this.artInfo.role = 1
+      }else{
+        this.artInfo.role = 2
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.actionSlot {
+  display: flex;
+  justify-content: center;
+}
+
+.ArtImg{
+  height: 100%;
+  width: 100%;
+}
+
+.ArtImg img{
+  width: 100px;
+  height: 80px;
+}
+</style>
+
--- web/admin/src/components/category/CateList.vue
+++ web/admin/src/components/category/CateList.vue
@@ -1,5 +1,234 @@
 <template>
   <div>
     <h1>分类列表页面</h1>
+    <a-card>
+      <a-row :gutter="20">
+        <a-col :span="4">
+          <a-button type="primary" @click="addCateVisible = true" @ok="addCateOk" @cancel="addCateCancel">New Cate</a-button>
+        </a-col>
+      </a-row>
+
+      <a-table 
+        rowKey="id"
+        :columns="columns"
+        :pagination="pagination"
+        :dataSource="catelist"
+        bordered
+        @change="handleTableChange"
+        >
+        <template slot="action" slot-scope="data">
+          <div class="actionSlot">
+            <a-button type="primary" style="margin-right:15px" @click="editCate(data.id)">Edit</a-button>
+            <a-button type="danger" style="margin-right:15px" @click="deleteCate(data.id)">Delete</a-button>
+          </div>
+        </template>
+      </a-table>
+    </a-card>
+
+    <!-- New Cate -->
+    <a-modal
+      title="New Cate"
+      :visible="addCateVisible"
+      @ok="addCateOk"
+      width="60%"
+      @cancel="addCateCancel"
+      closable
+      destroyOnClose
+    >
+      <a-form-model :model="newCate" :rules="addCateRules" ref="addCateRef">
+        <a-form-model-item label="Cate Name" prop="name">
+          <a-input v-model="newCate.name"></a-input>
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
+
+    <!--Edit Cate-->
+    <a-modal
+      title="Edit Cate"
+      :visible="editCateVisible"
+      closable
+      width="60%"
+      @ok="editCateOk"
+      @cancel="editCateCancel"
+    >
+      <a-form-model :model="cateInfo" :rules="cateRules" ref="addCateRef">
+        <a-form-model-item label="Cate Name" prop="name">
+          <a-input v-model="cateInfo.name"></a-input>
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
+
   </div>
 </template>
+
+
+<script>
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    width: '10%',
+    key: 'id',
+    align: 'center'
+  },
+  {
+    title: 'CateName',
+    dataIndex: 'name',
+    width: '20%',
+    key: 'name',
+    align: 'center'
+  },
+  {
+    title: 'Action',
+    width: '30%',
+    key: 'action',
+    scopedSlots: {customRender: 'action'},
+    align: 'center',
+  }
+]
+
+export default{
+  data(){
+    return {
+      pagination: {
+        pageSizeOptions: ['5', '10', '20'],
+        pageSize: 5,
+        total: 0,
+        showSizeChanger: true,
+        showTotal: (total) => `共${total}条`,
+      },
+      catelist: [],
+      columns,
+      queryParam: {
+        pagesize: 5,
+        pagenum: 1,
+      },
+      cateInfo: {
+        id: 0,
+        name: "",
+      },
+      newCate: {
+        name: "",
+      },
+      visible: false,
+      addCateVisible: false,
+      cateRules: {
+        name: [
+          {required:true, message: "Please input catename", trigger: "blur"},
+        ],
+      },
+      addCateRules: {
+        name: [
+          {required:true, message: "Please input catename", trigger: "blur"},
+        ],
+      },
+
+      // Edit Cate
+      editCateVisible: false,
+    }
+
+  },
+  created(){
+    this.getCateList()
+  },
+  methods: {
+    async getCateList(){
+      const {data: res} = await this.$http.get('category', {
+        params:{
+          pagesize: this.queryParam.pagesize,
+          pagenum: this.queryParam.pagenum,
+        },
+      })
+      if (res.status != 200) return this.$message.error(res.message)
+      this.catelist = res.data
+      this.pagination.total = res.total
+    },
+
+    // 重来
+    handleTableChange(pagination, filters, sorter){
+      var pager = { ...this.pagination }
+      pager.current = pagination.current
+      pager.pageSize = pagination.pageSize
+      this.queryParam.pagesize = pagination.pageSize
+      this.queryParam.pagenum = pagination.current
+
+      if(pagination.pageSize != this.pagination.pageSize){
+        this.queryParam.pagenum = 1
+        pager.current = 1
+      }
+      this.pagination = pager
+      this.getCateList()
+    },
+
+    // Delete Cate
+    async deleteCate(id){
+      this.$confirm({
+        title: 'Do you want to delete this cate?',
+        content: 'When clicked the OK button, there is no way back',
+        onOk: async () => {
+          const res = await this.$http.delete(`category/${id}`)
+          if (res.status != 200) return this.$message.error(res.message)
+          this.getCateList()
+          this.$message.success("Cate has been delete!")
+        },
+        onCancel() {},
+      });
+    },
+
+    // Add Cate
+    addCateCancel(){
+      this.$refs.addCateRef.resetFields()
+      this.addCateVisible = false
+      this.$message.info("Add Cancel")
+    },
+    addCateOk(){
+      this.$refs.addCateRef.validate(async (valid) => {
+        if(!valid) return this.$message.error("Invalid")
+        const {data: res} = await this.$http.post("category/add", {
+          name: this.newCate.name,
+        })
+        if (res.status !=200) return this.$message.error(res.message)
+        this.addCateVisible = false
+        this.$message.success("Add Cate Success")
+        await this.getCateList()
+      })
+    },
+
+    // Edit Cate
+    async editCate(id) {
+      this.editCateVisible = true
+      const {data: res} = await this.$http.get(`category/${id}`)
+      this.cateInfo = res.data
+      this.cateInfo.id = id
+    },
+    editCateCancel(){
+      this.$refs.addCateRef.resetFields()
+      this.editCateVisible = false
+      this.$message.info("Edit Cancel")
+    },
+    editCateOk(){
+      this.$refs.addCateRef.validate(async (valid) => {
+        if(!valid) return this.$message.error("Invalid")
+        const {data: res} = await this.$http.put(`category/${this.cateInfo.id}`, {
+          name: this.cateInfo.name,
+        })
+        console.log(res)
+        if (res.status !=200) return this.$message.error(res.message)
+        this.editCateVisible = false
+        this.$message.success("Edit Cate Success")
+        this.getCateList()
+      })
+
+    }
+  },
+}
+</script>
+
+<style scoped>
+.actionSlot {
+  display: flex;
+  justify-content: center;
+}
+</style>
+
